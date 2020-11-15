9c318d86832e2488148a5fce9d8c25226ecabf84
Jennifer Evans <jenevans3ten@gmail.com>
2020-11-15 14:39:15 -0500

    added navigation bar

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-(() => alert('Hello, World!'))();
+const appDiv = document.querySelector('.app');
+const appHeader = document.querySelector('.header');
+
+(() => {
+    navElement();
+})();
+
+function nav(){
+    return `
+    <div class="nav__section">
+        <h3 class="nav__section__art">New Art</h3>
+        <h3 class="nav__section__api">Harvard API</h3>
+    </div>
+    `
+}
+
+function navElement(){
+    appHeader.innerHTML= nav();
+}
