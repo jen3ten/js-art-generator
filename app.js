0bc4ce7791724a11006612619df887697f75144c
Jennifer Evans <jenevans3ten@gmail.com>
2020-11-15 15:01:40 -0500

    added styling, positioning to nav bar and body

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const appHeader = document.querySelector('.header');
 function nav(){
     return `
     <div class="nav__section">
-        <h3 class="nav__section__art">New Art</h3>
-        <h3 class="nav__section__api">Harvard API</h3>
+        <h3 class="nav__section__items nav__section__art">New Art</h3>
+        <h3 class="nav__section__items nav__section__api">Harvard API</h3>
     </div>
     `
 }
